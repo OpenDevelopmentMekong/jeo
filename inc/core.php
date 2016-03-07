f6cf15701ace9f9d34f556cfde3613ba0aaf583e
Huy Eng Heng <huyeng_heng@yahoo.com>
2016-03-07 16:52:31 +0700

    Updates core file to update the fullscript on Childtheme

--- inc/core.php
+++ inc/core.php
@@ -133,13 +133,18 @@ class JEO {
 		wp_enqueue_script('jeo.groups', get_template_directory_uri() . '/inc/js/groups.js', array('jeo'), '0.2.7');
 
 		wp_enqueue_script('jeo.geocode', get_template_directory_uri() . '/inc/js/geocode.js', array('jeo'), '0.0.5');
-		wp_enqueue_script('jeo.fullscreen', get_template_directory_uri() . '/inc/js/fullscreen.js', array('jeo'), '0.0.7');
-		if ( file_exists( STYLESHEETPATH . '/inc/js/filter-layers.js')){ 
+		if ( file_exists( STYLESHEETPATH . '/inc/js/fullscreen.js')){
+			wp_enqueue_script('jeo-odm.fullscreen', get_stylesheet_directory_uri() . '/inc/js/fullscreen.js',array('jeo'), '0.2.0');
+		}else {
+			wp_enqueue_script('jeo.fullscreen', get_template_directory_uri() . '/inc/js/fullscreen.js', array('jeo'), '0.0.7');
+		}
+
+		if ( file_exists( STYLESHEETPATH . '/inc/js/filter-layers.js')){
 			wp_enqueue_script('jeo-odm.filterLayers', get_stylesheet_directory_uri() . '/inc/js/filter-layers.js',array('jeo'), '0.2.0');
 		}else {
 			wp_enqueue_script('jeo.filterLayers', get_template_directory_uri() . '/inc/js/filter-layers
 			.js', array('jeo'), '0.1.3');
-		}	
+		}
 		wp_enqueue_script('jeo.ui', get_template_directory_uri() . '/inc/js/ui.js', array('jeo'), '0.0.9');
 		wp_enqueue_style('jeo', get_template_directory_uri() . '/inc/css/jeo.css', array(), '0.0.2');
 
